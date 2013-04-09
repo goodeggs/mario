424364c479f00faf61510e32590a663cb9be6080
Andreas Lind Petersen <andreas@one.com>
2013-04-09 10:05:46 +0200

    Release 0.2.0-patch3.

--- bowser.js
+++ bowser.js
@@ -4,9 +4,9 @@
   * MIT License | (c) Dustin Diaz 2011
   */
 !function (name, definition) {
-  if (typeof define == 'function') define(definition)
-  else if (typeof module != 'undefined' && module.exports) module.exports['browser'] = definition()
-  else this[name] = definition()
+  if (typeof define == 'function') define(definition()(navigator.userAgent))
+  else if (typeof module != 'undefined' && module.exports) module.exports = definition()
+  else this[name] = definition()(navigator.userAgent)
 }('bowser', function () {
   /**
     * navigator.userAgent =>
@@ -20,49 +20,57 @@
     * Android: "Mozilla/5.0 (Linux; U; Android 2.3.4; en-us; T-Mobile G2 Build/GRJ22) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
     * Touchpad: "Mozilla/5.0 (hp-tabled;Linux;hpwOS/3.0.5; U; en-US)) AppleWebKit/534.6 (KHTML, like Gecko) wOSBrowser/234.83 Safari/534.6 TouchPad/1.0"
     * PhantomJS: "Mozilla/5.0 (Macintosh; Intel Mac OS X) AppleWebKit/534.34 (KHTML, like Gecko) PhantomJS/1.5.0 Safari/534.34"
+    * IE10 on a touch-enabled device: "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; ARM; Trident/6.0; Touch)"
     */
 
-  var ua = navigator.userAgent
-    , t = true
-    , ie = /msie/i.test(ua)
-    , chrome = /chrome/i.test(ua)
-    , phantom = /phantom/i.test(ua)
-    , safari = /safari/i.test(ua) && !chrome && !phantom
-    , iphone = /iphone/i.test(ua)
-    , ipad = /ipad/i.test(ua)
-    , touchpad = /touchpad/i.test(ua)
-    , android = /android/i.test(ua)
-    , opera = /opera/i.test(ua)
-    , firefox = /firefox/i.test(ua)
-    , gecko = /gecko\//i.test(ua)
-    , seamonkey = /seamonkey\//i.test(ua)
-    , webkitVersion = /version\/(\d+(\.\d+)?)/i
-    , o
+  var t = true
 
-  function detect() {
+  function detect(ua) {
+    function getFirstMatch(regexp) {
+      var match = ua.match(regexp);
+      return match && match[1];
+    }
+
+    var ie = /msie/i.test(ua)
+      , chrome = /chrome/i.test(ua)
+      , phantom = /phantom/i.test(ua)
+      , safari = /safari/i.test(ua) && !chrome && !phantom
+      , iphone = /iphone/i.test(ua)
+      , ipad = /ipad/i.test(ua)
+      , touchpad = /touchpad/i.test(ua)
+      , android = /android/i.test(ua)
+      , opera = /opera/i.test(ua)
+      , firefox = /firefox/i.test(ua)
+      , gecko = /gecko\//i.test(ua)
+      , seamonkey = /seamonkey\//i.test(ua)
+      , webkitVersion = /version\/(\d+(\.\d+)?)/i
+      , o
 
     if (ie) return {
-        msie: t
-      , version: ua.match(/msie (\d+(\.\d+)?);/i)[1]
+        touch: /touch/i.test(ua)
+      , msie: t
+      , version: getFirstMatch(/msie (\d+(\.\d+)?);/i)
     }
     if (chrome) return {
         webkit: t
       , chrome: t
-      , version: ua.match(/chrome\/(\d+(\.\d+)?)/i)[1]
+      , version: getFirstMatch(/chrome\/(\d+(\.\d+)?)/i)
     }
     if (phantom) return {
         webkit: t
       , phantom: t
-      , version: ua.match(/phantomjs\/(\d+(\.\d+)+)/i)[1]
+      , version: getFirstMatch(/phantomjs\/(\d+(\.\d+)+)/i)
     }
     if (touchpad) return {
         webkit: t
+      , touch: true
       , touchpad: t
-      , version : ua.match(/touchpad\/(\d+(\.\d+)?)/i)[1]
+      , version: getFirstMatch(/touchpad\/(\d+(\.\d+)?)/i)
     }
     if (iphone || ipad) {
       o = {
           webkit: t
+        , touch: t
         , mobile: t
         , ios: t
         , iphone: iphone
@@ -70,59 +78,64 @@
       }
       // WTF: version is not part of user agent in web apps
       if (webkitVersion.test(ua)) {
-        o.version = ua.match(webkitVersion)[1]
+        o.version = getFirstMatch(webkitVersion)
       }
       return o
     }
     if (android) return {
         webkit: t
+      , touch: t
       , android: t
       , mobile: t
-      , version: ua.match(webkitVersion)[1]
+      , version: getFirstMatch(webkitVersion)
     }
     if (safari) return {
         webkit: t
       , safari: t
-      , version: ua.match(webkitVersion)[1]
+      , version: getFirstMatch(webkitVersion)
     }
     if (opera) return {
         opera: t
-      , version: ua.match(webkitVersion)[1]
+      , version: getFirstMatch(webkitVersion)
     }
     if (gecko) {
       o = {
           gecko: t
         , mozilla: t
-        , version: ua.match(/firefox\/(\d+(\.\d+)?)/i)[1]
+        , version: getFirstMatch(/firefox\/(\d+(\.\d+)?)/i)
       }
       if (firefox) o.firefox = t
       return o
     }
     if (seamonkey) return {
         seamonkey: t
-      , version: ua.match(/seamonkey\/(\d+(\.\d+)?)/i)[1]
+      , version: getFirstMatch(/seamonkey\/(\d+(\.\d+)?)/i)
     }
   }
 
-  var bowser = detect()
+  return function createBowser(ua) {
+    var bowser = detect(ua)
+    if (!bowser) {
+        return
+    }
 
-  // Graded Browser Support
-  // http://developer.yahoo.com/yui/articles/gbs
-  if ((bowser.msie && bowser.version >= 7) ||
-      (bowser.chrome && bowser.version >= 10) ||
-      (bowser.firefox && bowser.version >= 4.0) ||
-      (bowser.safari && bowser.version >= 5) ||
-      (bowser.opera && bowser.version >= 10.0)) {
-    bowser.a = t;
+    // Graded Browser Support
+    // http://developer.yahoo.com/yui/articles/gbs
+    if ((bowser.msie && bowser.version >= 7) ||
+        (bowser.chrome && bowser.version >= 10) ||
+        (bowser.firefox && bowser.version >= 4.0) ||
+        (bowser.safari && bowser.version >= 5) ||
+        (bowser.opera && bowser.version >= 10.0)) {
+      bowser.a = t;
+    }
+
+    else if ((bowser.msie && bowser.version < 7) ||
+        (bowser.chrome && bowser.version < 10) ||
+        (bowser.firefox && bowser.version < 4.0) ||
+        (bowser.safari && bowser.version < 5) ||
+        (bowser.opera && bowser.version < 10.0)) {
+      bowser.c = t
+    } else bowser.x = t
+    return bowser;
   }
-
-  else if ((bowser.msie && bowser.version < 7) ||
-      (bowser.chrome && bowser.version < 10) ||
-      (bowser.firefox && bowser.version < 4.0) ||
-      (bowser.safari && bowser.version < 5) ||
-      (bowser.opera && bowser.version < 10.0)) {
-    bowser.c = t
-  } else bowser.x = t
-
-  return bowser
 })
